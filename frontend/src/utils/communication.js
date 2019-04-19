3d08fe6e331d6a897fba4426524af6c0462da8e2
Sam Lazarus <tyu.sam@gmail.com>
2019-04-19 02:31:58 -0400

    Fixed issue with heroku frontend backend bridging

--- frontend/src/utils/communication.js
+++ frontend/src/utils/communication.js
@@ -1,7 +1,6 @@
 import config from '../config'
 
 export const fetchJSON = async (endpoint) => {
-  console.log(`fetching: ${config['backendAPI']}/${endpoint}`);
-  const response = await fetch(`${config['backendAPI']}/${endpoint}`);
+  const response = await fetch(endpoint);
   return response.json();
 };
