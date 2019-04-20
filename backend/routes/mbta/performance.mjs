42ae41b89fcac67021297a080a215e43a74d74d8
Sam Lazarus <tyu.sam@gmail.com>
2019-04-20 16:15:21 -0400

    Added server side validation to the performance routes.

--- backend/routes/mbta/performance.mjs
+++ backend/routes/mbta/performance.mjs
@@ -16,9 +16,21 @@ router.use(bodyParser.urlencoded({
 
 // routes for /api/mbta/performance
 
+const isValidDate = /[0-9]+(-[0-9]+){0,2}/;
+
+// checks if a date is valid
+const validate = (dateString) => isValidDate.test(dateString);
+
 // gets the performance rating for the givee date
 router.get('/:date', cors(), async (req, res) => {
   const date = req.params.date;
+  
+  if (!validate(date)) {
+    res.status(400);
+    res.send(`malformed date: "${date}"`)
+    return;
+  }
+  
   const performance = await getPerformanceForDate(date);
   res.json(performance);
 });
@@ -27,8 +39,28 @@ router.get('/:date', cors(), async (req, res) => {
 router.get('/range/:start/:end', cors(), async (req, res) => {
   const start = req.params.start;
   const end = req.params.end;
-  const performance = await getPerformanceForRange(start, end);
-  res.json(performance);
+  
+  const startValid = validate(start);
+  const endValid = validate(end);
+  
+  const errors = [];
+  
+  if (!startValid) errors.push(`malformed start date: "${start}"`);
+  if (!endValid) errors.push(`malformed end date "${end}"`);
+  
+  if (errors.length !== 0) {
+    res.status(400);
+    res.send(`invalid date range: ${errors.join(',')}`);
+    return;
+  }
+  
+  try {
+    const performance = await getPerformanceForRange(start, end);
+    res.json(performance);
+  } catch (e) {
+    res.status(400);
+    res.send(`${e.name}: ${e.message}`);
+  }
 });
 
 export default router;
