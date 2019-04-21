30808f45fc39d1b0e3abdc7154c5fcecdbb8eb90
Sam Lazarus <tyu.sam@gmail.com>
2019-04-21 13:16:05 -0400

    Added more dialogues and information

--- frontend/src/Visualizer.js
+++ frontend/src/Visualizer.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import * as V from 'victory';
 import { fetchJSON } from './utils/communication';
+import { toFormattedDateString } from './utils/date';
 
 const Visualizer = (props) => {
   const { classes, startDate, endDate, comparison } = props;
 
+  const [performData, setPerformData] = useState([]);
   const [compareData, setCompareData] = useState([]);
   const [initialized, setInitialized] = useState(false);
   
@@ -16,18 +18,31 @@ const Visualizer = (props) => {
       console.log('attempting to transform the data...');
       // transform the performance data 
       
+      let maximumComparison = Number.NEGATIVE_INFINITY;
       const compareData = Object.keys(compare).map((key) => {
         const date = Date.parse(key);
+        const floatKey = parseFloat(compare[key]);
+        if (floatKey > maximumComparison) {
+          maximumComparison = floatKey
+        }
         return {
           x: date,
-          y: +parseFloat(compare[key]).toFixed(2),
-          performance: +performance[key].toFixed(2)
+          y: +floatKey.toFixed(2),
+          performance: +performance[key].toFixed(2),
+          compare: +floatKey.toFixed(2)
         };
       });
       
-      console.log(compareData)
+      const performanceData = Object.keys(compare).map((key) => {
+        const date = Date.parse(key)
+        return {
+          x: date,
+          y: maximumComparison * performance[key]
+        };
+      })
 
       setCompareData(compareData);
+      setPerformData(performanceData);
       setInitialized(true);
     };
     
@@ -46,7 +61,13 @@ const Visualizer = (props) => {
       containerComponent={
         <V.VictoryVoronoiContainer
           voronoiDimension="x"
-          labels={(d) => `performance: ${d.performance}`}
+          labels={(d) => {
+            if (d.performance) {
+              return `date: ${toFormattedDateString(new Date(d.x))}\n` +
+                `performance: ${d.performance}\n` +
+                `${comparison}: ${d.compare}`;
+            }
+          }}
           labelComponent={
             <V.VictoryTooltip
               cornerRadius={0}
@@ -63,6 +84,27 @@ const Visualizer = (props) => {
           parent: { border: "1px solid #ccc"}
         }}
         data={compareData}
+      />
+      <V.VictoryLine
+        theme={V.VictoryTheme.material}
+        style={{
+          data: { stroke: 'red' },
+          parent: { border: "1px solid #ccc"}
+        }}
+        data={performData}
+      />
+      <V.VictoryLegend
+        x={110}
+        y={10}
+        gutter={20}
+        orientation="horizontal"
+        colorScale={['red', 'blue']}
+        style={{
+          border: { stroke: 'black' }
+        }}
+        data={[
+          { name: 'performance' }, { name: comparison }
+        ]}
       />
     </V.VictoryChart>
   );
